e0196327098f5e4a002d9a623ac1032efd45aed7
N1ghting4le <nikansoloveib@gmail.com>
2025-05-06 19:31:24 +0300

    base tour price validation constraints are modified

--- client/src/components/tourForm/schema.js
+++ client/src/components/tourForm/schema.js
@@ -2,17 +2,23 @@ import { object, string, number } from "yup";
 import textFields from "./textFields";
 
 const schema = object().shape({
-    ...Object.fromEntries(textFields.map(field => {
-        const { name, error } = field;
+  ...Object.fromEntries(
+    textFields.map((field) => {
+      const { name, error } = field;
 
-        return [name, error ? string().trim().required(error) : string().trim().notRequired()];
-    })),
-    departureCity: string().required("Вы не выбрали город"),
-    destinationCountry: string().required("Вы не выбрали страну"),
-    hotelTitle: string().required("Вы не выбрали отель"),
-    basePrice: number()
-                .typeError("Вы не установили начальную цену")
-                .positive("Значение должно быть положительным")
+      return [
+        name,
+        error ? string().trim().required(error) : string().trim().notRequired(),
+      ];
+    })
+  ),
+  departureCity: string().required("Вы не выбрали город"),
+  destinationCountry: string().required("Вы не выбрали страну"),
+  hotelTitle: string().required("Вы не выбрали отель"),
+  basePrice: number()
+    .typeError("Вы не установили начальную цену")
+    .min(50, "Минимальная цена - 50$")
+    .max(1200, "Максимальная цена - 1200$"),
 });
 
 export default schema;
